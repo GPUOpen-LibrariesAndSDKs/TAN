7dbe8687b183a2d93a29de567e37c2657cb4d190
Jason Stewart <Jason.Stewart@amd.com>
2016-08-16 13:39:04 -0400

    Add version header file for clFFT
    We don't use clFFT make files in this project, so this header has to be
    manually generated.

---
+++ tan/tanlibrary/src/clFFT-master/src/include/clFFT.version.h
@@ -0,0 +1,19 @@
+#ifndef CLFFT_VERSION_MAJOR
+#define CLFFT_VERSION_MAJOR 2
+#endif
+
+#ifndef CLFFT_VERSION_MINOR
+#define CLFFT_VERSION_MINOR 12
+#endif
+
+#ifndef CLFFT_VERSION_PATCH
+#define CLFFT_VERSION_PATCH 1
+#endif
+
+#define CLFFT_STATIC
+
+#define clfftVersionMajor CLFFT_VERSION_MAJOR
+
+#define clfftVersionMinor CLFFT_VERSION_MINOR
+
+#define clfftVersionPatch CLFFT_VERSION_PATCH
