02c98f9bbcf240080e020673d2a3c3bf850428a9
radeonprorender <radeonprorender.buildmaster@gmail.com>
2020-05-15 19:55:37 +0200

    buildmaster: automatic build version update to 16

--- tan/tanlibrary/include/TrueAudioNext.h
+++ tan/tanlibrary/include/TrueAudioNext.h
@@ -43,7 +43,7 @@
 #define TAN_VERSION_MAJOR          1
 #define TAN_VERSION_MINOR          3
 #define TAN_VERSION_RELEASE        1
-#define TAN_VERSION_BUILD          15
+#define TAN_VERSION_BUILD          16
 
 #define TAN_FULL_VERSION ( (uint64_t(TAN_VERSION_MAJOR) << 48ull) |   \
                            (uint64_t(TAN_VERSION_MINOR) << 32ull) |   \
