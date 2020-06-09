68e4990fd24132e60f651b92f72eb2ce70bf36f5
user <user@MacBook-Pro-user.local>
2020-06-09 18:13:23 +0300

    +add parameter to configure AMF_HOME and AMF build type

--- tan/build/cmake/generate-mac.sh
+++ tan/build/cmake/generate-mac.sh
@@ -4,5 +4,5 @@ rm -rf ./mac-gnumake
 mkdir mac-gnumake
 cd mac-gnumake
 #cmake .. -DCMAKE_PREFIX_PATH="/Applications/Qt5.6.3/5.6.3/clang_64" -DOpenCL_INCLUDE_DIR="../../../../thirdparty/OpenCL-Headers" -DOpenCL_LIBRARY="/System/Library/Frameworks/OpenCL.framework" -DPortAudio_DIR="../../../../../thirdparty/portaudio" -DDEFINE_AMD_OPENCL_EXTENSION=1
-cmake .. -DCMAKE_PREFIX_PATH="/Applications/Qt5.6.3/5.6.3/clang_64" -DOpenCL_INCLUDE_DIR="../../../../thirdparty/OpenCL-Headers" -DPortAudio_DIR="../../../../../thirdparty/portaudio" -DDEFINE_AMD_OPENCL_EXTENSION=1
+cmake .. -DCMAKE_PREFIX_PATH="/Applications/Qt5.6.3/5.6.3/clang_64" -DOpenCL_INCLUDE_DIR="../../../../thirdparty/OpenCL-Headers" -DPortAudio_DIR="../../../../../thirdparty/portaudio" -DDEFINE_AMD_OPENCL_EXTENSION=1 -DAMF_HOME="../../../../../amfOpen/amf" -DAMF_CORE_STATIC=1
 cd ..
